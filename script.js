72b506fee5aa47c32fff27b5be1918d1f22edfea
Pranali Pandhare <pranalipandhare2002@gmail.com>
2023-07-08 21:10:01 +0530

    new commit7

---
+++ script.js
@@ -0,0 +1,10 @@
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
