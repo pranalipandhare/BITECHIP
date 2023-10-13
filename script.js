60bb1a7e28d834d6d8fe20656e67458713ceea58
Pranali Pandhare <pranalipandhare2002@gmail.com>
2023-10-13 19:28:04 +0530

    new commit

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
 
 if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
 if (close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active'); 
-    })
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+let popup = document.getElementById("popup");
+
+function openPopup() {
+  popup.classList.add("open-popup");
+}
+
+function closePopup() {
+  popup.classList.remove("open-popup");
 }
