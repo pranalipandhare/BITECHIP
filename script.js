ca3a6af3bce2ba9432d4cd9b8f46d5f8e9494a46
student Pranali <pranalipandhare2002@gmail.com>
2024-01-11 23:56:04 +0530

    commit

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ function openPopup() {
 function closePopup() {
   popup.classList.remove("open-popup");
 }
+// Get the modal
+var modal = document.getElementById("id01");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
